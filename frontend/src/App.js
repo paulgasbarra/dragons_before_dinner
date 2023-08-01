9b18b1ae9ac7a8c7edfaa24922b8725ca1c41074
Paul Gasbarra <paulgasbarra@gmail.com>
2023-08-01 12:37:25 -0400

    add menu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Gameboard from './Components/Gameboard/Gameboard';
+import Menu from './Components/Menu/Menu';
 
 function App() {
   return (
     <div className="App">
       <h1>Dragons Before Dinner</h1>
-        <Gameboard />
+      <Menu />
+      <Gameboard />
     </div>
   );
 }
