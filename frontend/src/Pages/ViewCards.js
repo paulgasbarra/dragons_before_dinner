d9209e7afaa418baedcc28c8d6d972a5efa31b98
Paul Gasbarra <paulgasbarra@gmail.com>
2024-03-18 16:56:41 -0400

    delete dotenv

--- frontend/src/Pages/ViewCards.js
+++ frontend/src/Pages/ViewCards.js
@@ -1,39 +1,40 @@
 import { useEffect, useState } from "react";
 
 const ViewCards = () => {
-    const user_id = 0// get user id from session
-    const [loadedCards, setLoadedCards] = useState([]);
-    // fetch a list of cards that the user has created
-    useEffect(() => {
-        const fetchCards = async () => {
-            try {
-                const response = await fetch(
-                    `http://localhost:3000/api/cards`
-                );
-                console.log(response);
-                const data = await response.json();
-                setLoadedCards(data.cards);
-            } catch (err) {}
-        };
-        fetchCards();
-    },[]);
-    
-    return (
-        <div>
-            <h1>View Cards</h1>
-            <h2>Viewing {loadedCards.length} cards.</h2>
-            <ul>
-                {loadedCards && loadedCards.map(card => {
-                    return (
-                        <li>
-                            <h2>{card.name}</h2>
-                            <p>{card.description}</p>
-                        </li>
-                    );
-                })}
-            </ul>
-        </div>
-    )
+  const user_id = 0; // get user id from session
+  const [loadedCards, setLoadedCards] = useState([]);
+  // fetch a list of cards that the user has created
+  useEffect(() => {
+    const fetchCards = async () => {
+      try {
+        const response = await fetch(`http://localhost:3000/api/cards`);
+        console.log(response);
+        const data = await response.json();
+        setLoadedCards(data.cards);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchCards();
+  }, []);
+
+  return (
+    <div>
+      <h1>View Cards</h1>
+      <h2>Viewing {loadedCards.length} cards.</h2>
+      <ul>
+        {loadedCards &&
+          loadedCards.map((card) => {
+            return (
+              <li>
+                <h2>{card.name}</h2>
+                <p>{card.description}</p>
+              </li>
+            );
+          })}
+      </ul>
+    </div>
+  );
 };
 
 export default ViewCards;
